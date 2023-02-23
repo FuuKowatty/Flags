2bd44ca28aa6b272b7c241b35e60301e9251de7f
FuuKowatty <miedzio.2000@gmail.com>
2023-02-23 13:35:44 +0100

    added typescript to project

---
+++ src/components/Countries.tsx
@@ -0,0 +1,19 @@
+import Country from "./CountryCard";
+
+const Countries = ({ countries }) => {
+  const { data, isPending, error } = countries;
+
+  return (
+    <div className="w-full flex flex-wrap flex-1 justify-center gap-[50px] dark:text-dark-mode-text">
+      {isPending && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {data && data.length === 0 && <p>No Country</p>}
+      {data &&
+        data.map((country) => (
+          <Country key={country.name.common} country={country} />
+        ))}
+    </div>
+  );
+};
+
+export default Countries;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { HiMoon } from "react-icons/hi";
 
